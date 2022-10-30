e0fad17d2d904bcdde86722c546b40e1500656d9
RealKiller666 <forbusinessonly863@gmail.com>
2022-10-30 22:20:37 +0700

    Revert changes to prepare for the rewrite branch.

--- logingateway/client.py
+++ logingateway/client.py
@@ -3,15 +3,14 @@ import hashlib
 import urllib.parse
 
 from datetime import datetime
-from typing import Any, Callable, TypeVar, Dict, Optional
+from typing import Any, Callable
 
 from .io import GatewayIO
 from .utils import encodeToken
 from .model import Player, Ready
 
-CallT = TypeVar("CallT", bound=Callable[..., Any])
-
 __all__ = ("HuTaoLoginAPI",)
+
 
 class HuTaoLoginAPI:
     URL: str = "https://hutao-login-gateway.m307.dev"
@@ -33,7 +32,7 @@ class HuTaoLoginAPI:
         if not options.get("login_url") is None:
             self.URL_LOGIN = options.get("login_url")
 
-        self.__decoractors: Dict[str, Callable] = {} 
+        self.__decorector = {}
 
         self.__client_id = client_id
         self.__client_secret = client_secret
@@ -82,66 +81,64 @@ class HuTaoLoginAPI:
 
         await self.callback(event, data)
 
-    @staticmethod
     async def connect_error(namespace: str, data: dict):
         raise Exception(data["message"])
 
     async def callback(self, event: str, data):
-        func = self.__decoractors[event] if event in self.__decoractors else self.null
-        
+        func: Callable = self.__decorector[event] if event in self.__decorector else self.null
         if not asyncio.iscoroutinefunction(func):
             return
 
         asyncio.ensure_future(func(data))
 
-    async def null(self) -> None:
+    async def null(self):
         return
 
-    def ready(self, cb: CallT) -> Optional[Callable[[CallT], CallT]]:
-        def _callback(func: CallT) -> CallT:
-            self.__decoractors["ready"] = func
+    def ready(self, cb: Callable = None):
+        def _callback(func: Callable):
+            self.__decorector["ready"] = func
             return func
 
         if cb:
-            self.__decoractors["ready"] = cb
+            self.__decorector["ready"] = cb
             return 
 
         return _callback
 
-    def player(self, cb: CallT = None) -> Optional[Callable[[CallT], CallT]]:
-        def _callback(func: CallT) -> CallT:
-            self.__decoractors["player"] = func
+    def player(self, cb: Callable = None):
+        def _callback(func: Callable):
+            self.__decorector["player"] = func
             return func
 
         if cb:
-            self.__decoractors["player"] = cb
+            self.__decorector["player"] = cb
             return 
         
         return _callback
 
-    def error(self, cb: CallT = None) -> Optional[Callable[[CallT], CallT]]:
-        def _callback(func: CallT) -> CallT:
-            self.__decoractors["connect_error"] = func
+    def error(self, cb: Callable = None):
+        def _callback(func: Callable):
+            self.__decorector["connect_error"] = func
             return func
 
         if cb:
-            self.__decoractors["connect_error"] = cb
+            self.__decorector["connect_error"] = cb
             return 
             
         return _callback
 
-    def disconnect(self, cb: CallT = None) -> Optional[Callable[[CallT], CallT]]:
-        if cb:
-            self.__decoractors["disconnect"] = cb
-#            return cb #I'll test again if this is required
-        
-        def _callback(func: CallT) -> CallT:
-            self.__decoractors["disconnect"] = func
+    def disconnect(self, cb: Callable = None):
+        def _callback(func: Callable):
+            self.__decorector["disconnect"] = func
             return func
+
+        if cb:
+            self.__decorector["disconnect"] = cb
+            return 
 
         return _callback
 
-    def start(self) -> None:
+    def start(self):
         asyncio.ensure_future(self._start())
 
     async def _start(self):
